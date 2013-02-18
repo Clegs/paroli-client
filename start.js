5c0355c27df27d50e032bbe43c3bfdedf69990ab
Clegs <jmail@calebgo.com>
2013-02-17 22:57:04 -0800

    Allowed client to read input

--- start.js
+++ start.js
@@ -18,14 +18,6 @@ process.stdin.on('data', function(d) {
 	onData(d);
 });
 
-process.stdin.on('end', function() {
-	process.stdout.write('end');
-});
-
-// Get required information
-process.stdout.write('User: ');
-onData = getUser;
-
 var getUser = function(d) {
 	user = d.trim();
 	console.log('Username set to %s', user);
@@ -36,6 +28,25 @@ var getUser = function(d) {
 var getPass = function(d) {
 	pass = d.trim();
 	console.log('Password set to %s', pass);
+	onData = getServer;
+	process.stdout.write('Server: ');
+};
+
+var getServer = function(d) {
+	server = d.trim();
+	console.log('Server set to %s', server);
+	onData = getPort;
+	process.stdout.write('Port: ');
+};
+
+var getPort = function(d) {
+	port = parseInt(d.trim());
+	console.log('Port set to %d', port);
 	onData = null;
 };
+
+// Get required information
+process.stdout.write('User: ');
+onData = getUser;
+
 
